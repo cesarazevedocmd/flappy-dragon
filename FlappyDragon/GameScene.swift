061c9e6dea3ac96fae46784bc090a02e13488ad2
César Alves <Cesar.AlvesdeAzevedo-ext@ab-Inbev.com>
2020-12-08 18:31:54 -0300

    Creating objects and animations

--- FlappyDragon/GameScene.swift
+++ FlappyDragon/GameScene.swift
@@ -10,19 +10,107 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var floor: SKSpriteNode!
+    var intro: SKSpriteNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var gameArea: CGFloat = 410.0
+    var velocity: Double = 100.0
+    var gameFinished: Bool = false
+    var gameStarted: Bool = false
+    var restart: Bool = false
+    var score: Int = 0
+    var flyForce: CGFloat = 30
     
     override func didMove(to view: SKView) {
+        addBackground()
+        addFloor()
+        addIntro()
+        addPlayer()
+        moveFloor()
+    }
+    
+    func addBackground(){
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size.width = background.size.width * 4
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        addChild(background)
+    }
+    
+    func addFloor(){
+        floor = SKSpriteNode(imageNamed: "floor")
+        floor.position = CGPoint(x: floor.size.width/2, y: self.size.height - gameArea - floor.size.height/2)
+        floor.zPosition = 2
+        addChild(floor)
+    }
+    
+    func addIntro(){
+        intro = SKSpriteNode(imageNamed: "intro")
+        intro.position = CGPoint(x: size.width/2, y: self.size.height - 210)
+        intro.zPosition = 3
+        addChild(intro)
+    }
+    
+    func addPlayer(){
+        player = SKSpriteNode(imageNamed: "player1")
+        player.position = CGPoint(x: 80, y: self.size.height - gameArea/2)
+        player.zPosition = 4
         
-      
+        var playerTextures = [SKTexture]()
+        for i in 1...4 {
+            playerTextures.append(SKTexture(imageNamed: "player\(i)"))
+        }
+        
+        let animationAction = SKAction.animate(with: playerTextures, timePerFrame: 0.09)
+        let repeatAction = SKAction.repeatForever(animationAction)
+        player.run(repeatAction)
+        
+        addChild(player)
+    }
+    
+    func moveFloor(){
+        let duration = Double(floor.size.width/2)/velocity
+        let moveFloorAction = SKAction.moveBy(x: -floor.size.width/2, y: 0, duration: duration)
+        let resetXAction = SKAction.moveBy(x: floor.size.width/2, y: 0, duration: 0)
+        let sequenceAction = SKAction.sequence([moveFloorAction, resetXAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        floor.run(repeatAction)
+    }
+    
+    func addScore(){
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.fontSize = 94
+        scoreLabel.text = "\(score)"
+        scoreLabel.zPosition = 5
+        scoreLabel.position = CGPoint(x: size.width/2, y: size.height - 100)
+        scoreLabel.alpha = 0.8
+        addChild(scoreLabel)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
+        if !gameFinished {
+            if !gameStarted {
+                intro.removeFromParent()
+                gameStarted = true
+                
+                player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2 - 10)
+                player.physicsBody?.isDynamic = true
+                player.physicsBody?.allowsRotation = true
+                player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+                
+                gameStarted = true
+            }  else {
+                player.physicsBody?.velocity = CGVector.zero
+                player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+            }
+        }
     }
         
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if gameStarted {
+            let yVelocity = player.physicsBody!.velocity.dy * 0.001 as CGFloat
+            player.zRotation = yVelocity
+        }
     }
 }
